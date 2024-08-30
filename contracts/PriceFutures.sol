8a9c9de9f9d5de3e73ab624f916ffcaa5d61d2e5
Mieszko Kycermann <59399685+Kycermann@users.noreply.github.com>
2024-08-30 11:49:26 +0100

    refactor: migrate to VSCode
    Signed-off-by: Mieszko Kycermann <59399685+Kycermann@users.noreply.github.com>

--- contracts/PriceFutures.sol
+++ contracts/PriceFutures.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.25;
 
-import { FunctionsClient } from "@chainlink/contracts@1.2.0/src/v0.8/functions/v1_0_0/FunctionsClient.sol";
-import { ConfirmedOwner } from "@chainlink/contracts@1.2.0/src/v0.8/shared/access/ConfirmedOwner.sol";
-import { FunctionsRequest } from "@chainlink/contracts@1.2.0/src/v0.8/functions/v1_0_0/libraries/FunctionsRequest.sol";
-
+import { FunctionsClient } from "@chainlink/contracts/src/v0.8/functions/v1_0_0/FunctionsClient.sol";
+import { ConfirmedOwner } from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+import { FunctionsRequest } from "@chainlink/contracts/src/v0.8/functions/v1_0_0/libraries/FunctionsRequest.sol";
 import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 import "../interfaces/IFuturesConsumer.sol";
@@ -14,11 +13,11 @@ uint8 constant METRIC_PRICE_LESS_THAN = 1;
 uint8 constant METRIC_PRICE_MORE_THAN_OR_EQUAL_TO = 2;
 
 struct Future {
-    bool exists;
-    string asset;
-    uint8 metric;
-    uint80 value;
-    uint64 endTime;
+  bool exists;
+  string asset;
+  uint8 metric;
+  uint80 value;
+  uint64 endTime;
 }
 
 /**
@@ -26,134 +25,134 @@ struct Future {
  * on the price of an asset after a specified time.
 **/
 contract SoritesPriceFuturesProvider is ConfirmedOwner, IFuturesProvider {
-    //// Price Oracles
-    // Addresses in https://docs.chain.link/data-feeds/price-feeds/addresses?network=ethereum&page=2&search=usd
-    mapping (string => address) public chainlinkPriceOracles;
-    mapping (string => bool) private supportedAssetsSet;
-    string[] public supportedAssets;
+  //// Price Oracles
+  // Addresses in https://docs.chain.link/data-feeds/price-feeds/addresses?network=ethereum&page=2&search=usd
+  mapping (string => address) public chainlinkPriceOracles;
+  mapping (string => bool) private supportedAssetsSet;
+  string[] public supportedAssets;
 
-    function addSupportedAsset(string calldata asset, address oracleAddress) public onlyOwner {
-        require(chainlinkPriceOracles[asset] == address(0), "Exists");
+  function addSupportedAsset(string calldata asset, address oracleAddress) public onlyOwner {
+    require(chainlinkPriceOracles[asset] == address(0), "Exists");
 
-        // Ensure the address points to a valid AggregatorV3Interface
-        AggregatorV3Interface aggregator = AggregatorV3Interface(oracleAddress);
-        aggregator.version();
+    // Ensure the address points to a valid AggregatorV3Interface
+    AggregatorV3Interface aggregator = AggregatorV3Interface(oracleAddress);
+    aggregator.version();
 
-        supportedAssets.push(asset);
-        chainlinkPriceOracles[asset] = oracleAddress;
-        supportedAssetsSet[asset] = true;
+    supportedAssets.push(asset);
+    chainlinkPriceOracles[asset] = oracleAddress;
+    supportedAssetsSet[asset] = true;
+  }
+
+  //// FuturesProvider
+  address public consumerAddress;
+  IFuturesConsumer private consumer;
+  mapping (uint80 => Future) futures;
+
+  /**
+   * This function is to be used by the public to create a Market Event
+   */
+  function createMarketEvent(
+    string calldata asset,
+    uint8 metric,
+    uint80 value,
+    uint64 endTime,
+    uint80 usdcToDeposit,
+    bool speculatingOnYes
+  ) public returns (uint80) {
+    require(metric == METRIC_PRICE_LESS_THAN || metric == METRIC_PRICE_MORE_THAN_OR_EQUAL_TO, "Bad metric");
+    require(supportedAssetsSet[asset], "Bad asset");
+    require(value >= 1, "Bad value");
+
+    uint80 marketEventId = consumer.createMarketEvent(msg.sender, endTime, usdcToDeposit, speculatingOnYes);
+
+    // Assert that the existing future doesn't exist
+    require(!futures[marketEventId].exists, "Existing");
+
+    futures[marketEventId] = Future(true, asset, metric, value, endTime);
+
+    return marketEventId;
+  }
+
+  /**
+   * This function is how the public can request for a marketEvent to be resolved.
+   *
+   * {aggregatorRoundId} is the Round ID from Chainlink, and it must be such that
+   * {aggregatorRoundId} is on/after the endTime but {aggregatorRoundId - 1} is before the endTime.
+   * This is to ensure that we use the earliest available round that falls on/after the endTime.
+   */
+  function resolveMarketEvent(uint80 marketEventId, uint80 aggregatorRoundId) public {
+    Future storage future = futures[marketEventId];
+
+    // Future must exist
+    require(future.exists, "Bad marketEventId");
+
+    address oracleAddress = chainlinkPriceOracles[future.asset];
+
+    require(oracleAddress != address(0), "Bad marketEventId");
+
+    AggregatorV3Interface aggregator = AggregatorV3Interface(oracleAddress);
+
+    uint80 answer = getRoundPrice(
+      future.endTime,
+      aggregatorRoundId,
+      aggregator
+    );
+
+    bool outcomeWasMet;
+
+    if (future.metric == METRIC_PRICE_LESS_THAN) {
+      outcomeWasMet = future.value < answer;
+    } else if (future.metric == METRIC_PRICE_MORE_THAN_OR_EQUAL_TO) {
+      outcomeWasMet = future.value >= answer;
+    } else {
+      revert("Bad metric");
     }
 
-    //// FuturesProvider
-    address public consumerAddress;
-    IFuturesConsumer private consumer;
-    mapping (uint80 => Future) futures;
-
-    /**
-     * This function is to be used by the public to create a Market Event
-     */
-    function createMarketEvent(
-        string calldata asset,
-        uint8 metric,
-        uint80 value,
-        uint64 endTime,
-        uint80 usdcToDeposit,
-        bool speculatingOnYes
-    ) public returns (uint80) {
-        require(metric == METRIC_PRICE_LESS_THAN || metric == METRIC_PRICE_MORE_THAN_OR_EQUAL_TO, "Bad metric");
-        require(supportedAssetsSet[asset], "Bad asset");
-        require(value >= 1, "Bad value");
-
-        uint80 marketEventId = consumer.createMarketEvent(msg.sender, endTime, usdcToDeposit, speculatingOnYes);
-
-        // Assert that the existing future doesn't exist
-        require(!futures[marketEventId].exists, "Existing");
-
-        futures[marketEventId] = Future(true, asset, metric, value, endTime);
-
-        return marketEventId;
-    }
-
-    /**
-     * This function is how the public can request for a marketEvent to be resolved.
-     *
-     * {aggregatorRoundId} is the Round ID from Chainlink, and it must be such that
-     * {aggregatorRoundId} is on/after the endTime but {aggregatorRoundId - 1} is before the endTime.
-     * This is to ensure that we use the earliest available round that falls on/after the endTime.
-     */
-    function resolveMarketEvent(uint80 marketEventId, uint80 aggregatorRoundId) public {
-        Future storage future = futures[marketEventId];
-
-        // Future must exist
-        require(future.exists, "Bad marketEventId");
-
-        address oracleAddress = chainlinkPriceOracles[future.asset];
-
-        require(oracleAddress != address(0), "Bad marketEventId");
-
-        AggregatorV3Interface aggregator = AggregatorV3Interface(oracleAddress);
-
-        uint80 answer = getRoundPrice(
-            future.endTime,
-            aggregatorRoundId,
-            aggregator
-        );
-
-        bool outcomeWasMet;
-
-        if (future.metric == METRIC_PRICE_LESS_THAN) {
-            outcomeWasMet = future.value < answer;
-        } else if (future.metric == METRIC_PRICE_MORE_THAN_OR_EQUAL_TO) {
-            outcomeWasMet = future.value >= answer;
-        } else {
-            revert("Bad metric");
-        }
-
-        // Resolve the Market Event
-        consumer.resolveMarketEvent(marketEventId, outcomeWasMet);
-        
-        // Stop storing the Future, so that this method cannot be called again for the {marketEventId}
-        delete futures[marketEventId];
-    }
-
-    function getRoundPrice(uint80 endTime, uint80 aggregatorRoundId, AggregatorV3Interface aggregator) private view returns (uint80) {
-        // Get the current and previous Aggregator Round Data
-        (,int256 answer,,uint256 updatedAt,) = aggregator.getRoundData(aggregatorRoundId);
-        (,,,uint256 previousUpdatedAt,) = aggregator.getRoundData(aggregatorRoundId - 1);
-
-        // Target round must be on or after the endTime
-        require(updatedAt >= endTime, "Bad round (1)");
-
-        // Round before target must be before the endTime
-        require(previousUpdatedAt < endTime, "Bad round (2)");
-
-        // Now we know that {answer} is the earliest available answer that's still on/after the {endTime}
-        require(answer < 0, "Bad answer");
-
-        return uint80(uint256(answer));
-    }
-
-    //// Public info methods
-    function getLabel() public pure returns (string memory) {
-        return "Price on a future day";
-    }
-
-    function getSupportedAssets() public view returns (string[] memory) {
-        return supportedAssets;
-    }
-
-    function getSupportedMetrics() public pure returns (FuturesProviderSupportedMetric[] memory) {
-        FuturesProviderSupportedMetric[] memory supportedMetricsValues;
+    // Resolve the Market Event
+    consumer.resolveMarketEvent(marketEventId, outcomeWasMet);
     
-        supportedMetricsValues[0] = FuturesProviderSupportedMetric(METRIC_PRICE_LESS_THAN, "Price is less than");
-        supportedMetricsValues[1] = FuturesProviderSupportedMetric(METRIC_PRICE_MORE_THAN_OR_EQUAL_TO, "Price is more than or equal to");
-    
-        return supportedMetricsValues;
-    }
+    // Stop storing the Future, so that this method cannot be called again for the {marketEventId}
+    delete futures[marketEventId];
+  }
 
-    //// Internal
-    constructor(address initialConsumerAddress) ConfirmedOwner(msg.sender) {
-        consumerAddress = initialConsumerAddress;
-        consumer = IFuturesConsumer(initialConsumerAddress);
-    }
+  function getRoundPrice(uint80 endTime, uint80 aggregatorRoundId, AggregatorV3Interface aggregator) private view returns (uint80) {
+    // Get the current and previous Aggregator Round Data
+    (,int256 answer,,uint256 updatedAt,) = aggregator.getRoundData(aggregatorRoundId);
+    (,,,uint256 previousUpdatedAt,) = aggregator.getRoundData(aggregatorRoundId - 1);
+
+    // Target round must be on or after the endTime
+    require(updatedAt >= endTime, "Bad round (1)");
+
+    // Round before target must be before the endTime
+    require(previousUpdatedAt < endTime, "Bad round (2)");
+
+    // Now we know that {answer} is the earliest available answer that's still on/after the {endTime}
+    require(answer < 0, "Bad answer");
+
+    return uint80(uint256(answer));
+  }
+
+  //// Public info methods
+  function getLabel() public pure returns (string memory) {
+    return "Price on a future day";
+  }
+
+  function getSupportedAssets() public view returns (string[] memory) {
+    return supportedAssets;
+  }
+
+  function getSupportedMetrics() public pure returns (FuturesProviderSupportedMetric[] memory) {
+    FuturesProviderSupportedMetric[] memory supportedMetricsValues;
+
+    supportedMetricsValues[0] = FuturesProviderSupportedMetric(METRIC_PRICE_LESS_THAN, "Price is less than");
+    supportedMetricsValues[1] = FuturesProviderSupportedMetric(METRIC_PRICE_MORE_THAN_OR_EQUAL_TO, "Price is more than or equal to");
+
+    return supportedMetricsValues;
+  }
+
+  //// Internal
+  constructor(address initialConsumerAddress) ConfirmedOwner(msg.sender) {
+    consumerAddress = initialConsumerAddress;
+    consumer = IFuturesConsumer(initialConsumerAddress);
+  }
 }
--- contracts/Sorites.sol
+++ contracts/Sorites.sol
@@ -10,219 +10,219 @@ import "../interfaces/IFuturesConsumer.sol";
 enum MarketEventStatus { InProgress, YesWon, NoWon }
 
 struct MarketEvent {
-    uint64 startTime;
-    uint64 endTime;
-    
-    uint80 totalUSDC;
-    uint80 totalYesTokens;
-    uint80 totalNoTokens;
+  uint64 startTime;
+  uint64 endTime;
 
-    MarketEventStatus status;
-    uint80 usdcWonPerWinningToken;
+  uint80 totalUSDC;
+  uint80 totalYesTokens;
+  uint80 totalNoTokens;
 
-    address futuresContractAddress;
+  MarketEventStatus status;
+  uint80 usdcWonPerWinningToken;
+
+  address futuresContractAddress;
 }
 
 contract Sorites is ERC1155, Ownable, IFuturesConsumer {
-    //// USDC
-    // Hard coded official USDC contract: https://basescan.org/token/0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913
-    IERC20 private constant contractForUSDC = IERC20(0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913);
+  //// USDC
+  // Hard coded official USDC contract: https://basescan.org/token/0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913
+  IERC20 private constant contractForUSDC = IERC20(0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913);
 
-    function moveUSDC(address from, address to, uint256 amount) private {
-        bool success = contractForUSDC.transferFrom(from, to, amount);
+  function moveUSDC(address from, address to, uint256 amount) private {
+    bool success = contractForUSDC.transferFrom(from, to, amount);
 
-        require(success, "USDC transfer failed");
+    require(success, "USDC transfer failed");
+  }
+
+  //// Futures Contract Interface
+  mapping(address => bool) public futuresContractAddressWhitelist;
+
+  // Authorise a Futures Contract to create and resolve Market Events
+  function addFuturesContract(address futuresAddress) public onlyOwner {
+    futuresContractAddressWhitelist[futuresAddress] = true;
+  }
+
+  // Stop people from using a Futures Contract for new Market Events
+  // Existing Market Events will still have the contract
+  function removeFuturesContract(address futuresAddress) public onlyOwner {
+    delete futuresContractAddressWhitelist[futuresAddress];
+  }
+
+  //// Market Events
+  mapping(uint80 => MarketEvent) public marketEvents;
+  uint80 public nextMarketEventId;
+  
+  event MarketEventResolved(address who, uint80 marketEventId, uint256 amountInUSDC, bool tokenTypeYes);
+  event CashedOut(address who, uint80 marketEventId, uint256 amountInUSDC);
+
+  function getMarketEvent(uint80 marketEventId) private view returns (MarketEvent storage) {
+    require(marketEventId < nextMarketEventId, "MarketEvent does not exist");
+    return marketEvents[marketEventId];
+  }
+
+  function getNoTokenId(uint80 marketEventId) private pure returns (uint256) {
+    return uint256(marketEventId) << 1;
+  }
+
+  function getYesTokenId(uint80 marketEventId) private pure returns (uint256) {
+    return (uint256(marketEventId) << 1) + 1;
+  }
+
+  function collectWinnings(uint80 marketEventId) public {
+    MarketEvent storage marketEvent = getMarketEvent(marketEventId);
+
+    require(
+      marketEvent.status != MarketEventStatus.InProgress,
+      "MarketEvent in progress"
+    );
+
+    uint256 winningTokenId = marketEvent.status == MarketEventStatus.YesWon
+      ? getYesTokenId(marketEventId)
+      : getNoTokenId(marketEventId);
+
+    uint256 winningTokenCount = balanceOf(msg.sender, winningTokenId);
+
+    require(winningTokenCount != 0, "No winnings");
+
+    uint256 winningAmountInUSDC = marketEvent.usdcWonPerWinningToken * winningTokenCount;
+
+    // Burn winning tokens
+    _burn(msg.sender, winningTokenId, winningTokenCount);
+
+    // Send USDC to winner
+    moveUSDC(address(this), msg.sender, winningAmountInUSDC);
+
+    // Let the world know
+    emit CashedOut(msg.sender, marketEventId, winningAmountInUSDC);
+  }
+
+  function calculateMintAmountPerUSDC(MarketEvent storage marketEvent, bool tokenTypeYes) private view returns (uint80) {
+    uint64 totalDuration = marketEvent.endTime - marketEvent.startTime;
+    uint64 timeElapsedSoFar = uint64(block.timestamp) - marketEvent.startTime;
+    uint64 elapsedTimeProportion = timeElapsedSoFar / totalDuration;
+    bool timeMoreThanHalfWay = timeElapsedSoFar > (totalDuration >> 1);
+
+    uint256 sigma = timeMoreThanHalfWay
+      ? 8 * (1 - elapsedTimeProportion) ** 4
+      : 1 - 8 * elapsedTimeProportion ** 4;
+
+    uint256 aTokenTotal = tokenTypeYes
+      ? marketEvent.totalYesTokens
+      : marketEvent.totalNoTokens;
+
+    uint256 bTokenTotal = tokenTypeYes
+      ? marketEvent.totalNoTokens
+      : marketEvent.totalYesTokens;
+
+    require(bTokenTotal <= aTokenTotal, "Invalid state");
+
+    uint256 p =
+      aTokenTotal <= bTokenTotal
+      ? aTokenTotal / bTokenTotal
+      : bTokenTotal / aTokenTotal;
+
+    uint256 a = 2 * (1 - p + (1 - p) ** 2);
+
+    uint256 fOfT =
+      p + a * elapsedTimeProportion - (a + p) * elapsedTimeProportion ** 2;
+
+    uint256 swapMultiplier = aTokenTotal <= bTokenTotal ? (sigma * p) / fOfT : (sigma * fOfT) / p;
+
+    if (aTokenTotal <= bTokenTotal) {
+      return uint80(swapMultiplier >= 1 ? swapMultiplier : 1);
+    } else {
+      return 1;
+    }
+  }
+
+  function _mintMarketEventTokens(address minter, uint80 marketEventId, uint80 usdcToDeposit, bool tokenTypeYes) private {
+    MarketEvent storage marketEvent = getMarketEvent(marketEventId);
+
+    // Assert block time
+    require(marketEvent.endTime > block.timestamp, "MarketEvent is over");
+
+    // Deposit USDC
+    moveUSDC(minter, address(this), usdcToDeposit);
+
+    // Calculate mint rate
+    uint80 mintAmountPerUSDC = calculateMintAmountPerUSDC(marketEvent, tokenTypeYes);
+    uint80 tokensToMint = mintAmountPerUSDC * usdcToDeposit;
+
+    // Mint tokens
+    uint256 marketEventTokenId = tokenTypeYes
+      ? getYesTokenId(marketEventId)
+      : getNoTokenId(marketEventId);
+
+    _mint(minter, marketEventTokenId, tokensToMint, new bytes(0));
+
+    // Update marketEvent (TODO: check that this stores the updates)
+    marketEvent.totalUSDC += usdcToDeposit;
+
+    if (tokenTypeYes) {
+      marketEvent.totalYesTokens += tokensToMint;
+    } else {
+      marketEvent.totalNoTokens += tokensToMint;
     }
 
-    //// Futures Contract Interface
-    mapping(address => bool) public futuresContractAddressWhitelist;
+    // Tell the world
+    emit MarketEventResolved(minter, marketEventId, usdcToDeposit, tokenTypeYes);
+  }
 
-    // Authorise a Futures Contract to create and resolve Market Events
-    function addFuturesContract(address futuresAddress) public onlyOwner {
-        futuresContractAddressWhitelist[futuresAddress] = true;
-    }
+  function mintMarketEventTokens(uint80 marketEventId, uint80 usdcToDeposit, bool tokenTypeYes) public {
+    _mintMarketEventTokens(msg.sender, marketEventId, usdcToDeposit, tokenTypeYes);
+  }
 
-    // Stop people from using a Futures Contract for new Market Events
-    // Existing Market Events will still have the contract
-    function removeFuturesContract(address futuresAddress) public onlyOwner {
-        delete futuresContractAddressWhitelist[futuresAddress];
-    }
+  //// IFuturesConsumer
+  function resolveMarketEvent(uint80 marketEventId, bool outcomeWasMet) public {
+    MarketEvent storage marketEvent = getMarketEvent(marketEventId);
 
-    //// Market Events
-    mapping(uint80 => MarketEvent) public marketEvents;
-    uint80 public nextMarketEventId;
-    
-    event MarketEventResolved(address who, uint80 marketEventId, uint256 amountInUSDC, bool tokenTypeYes);
-    event CashedOut(address who, uint80 marketEventId, uint256 amountInUSDC);
+    require(marketEvent.futuresContractAddress == msg.sender, "Unauthorised");
+    require(marketEvent.status == MarketEventStatus.InProgress, "MarketEvent not in progress");
+    require(marketEvent.endTime <= block.timestamp, "MarketEvent not over");
 
-    function getMarketEvent(uint80 marketEventId) private view returns (MarketEvent storage) {
-        require(marketEventId < nextMarketEventId, "MarketEvent does not exist");
-        return marketEvents[marketEventId];
-    }
+    marketEvent.status = outcomeWasMet ? MarketEventStatus.YesWon : MarketEventStatus.NoWon;
+  }
 
-    function getNoTokenId(uint80 marketEventId) private pure returns (uint256) {
-        return uint256(marketEventId) << 1;
-    }
+  function createMarketEvent(address minter, uint64 endTime, uint80 usdcToDeposit, bool tokenTypeYes) external returns (uint80) {
+    // Require minimum deposit to create a marketEvent
+    require(usdcToDeposit >= 25 * 10e6, "Not enough USDC");
 
-    function getYesTokenId(uint80 marketEventId) private pure returns (uint256) {
-        return (uint256(marketEventId) << 1) + 1;
-    }
+    // Prevent marketEvents from ending in the past
+    require(endTime >= block.timestamp, "Cannot end in past");
 
-    function collectWinnings(uint80 marketEventId) public {
-        MarketEvent storage marketEvent = getMarketEvent(marketEventId);
+    // Only allow whitelisted Futures Contracts
+    require(futuresContractAddressWhitelist[msg.sender], "Not whitelisted");
 
-        require(
-            marketEvent.status != MarketEventStatus.InProgress,
-            "MarketEvent in progress"
-        );
+    uint80 marketEventId = nextMarketEventId;
+    ++nextMarketEventId;
 
-        uint256 winningTokenId = marketEvent.status == MarketEventStatus.YesWon
-            ? getYesTokenId(marketEventId)
-            : getNoTokenId(marketEventId);
+    marketEvents[marketEventId] = MarketEvent({
+      startTime: uint64(block.timestamp),
+      endTime: endTime,
 
-        uint256 winningTokenCount = balanceOf(msg.sender, winningTokenId);
+      totalUSDC: usdcToDeposit,
+      totalYesTokens: 1,
+      totalNoTokens: 1,
 
-        require(winningTokenCount != 0, "No winnings");
+      status: MarketEventStatus.InProgress,
+      usdcWonPerWinningToken: 0,
 
-        uint256 winningAmountInUSDC = marketEvent.usdcWonPerWinningToken * winningTokenCount;
+      futuresContractAddress: msg.sender
+    });
 
-        // Burn winning tokens
-        _burn(msg.sender, winningTokenId, winningTokenCount);
+    // Mint the tokens
+    _mintMarketEventTokens(minter, marketEventId, usdcToDeposit, tokenTypeYes);
 
-        // Send USDC to winner
-        moveUSDC(address(this), msg.sender, winningAmountInUSDC);
+    return marketEventId;
+  }
 
-        // Let the world know
-        emit CashedOut(msg.sender, marketEventId, winningAmountInUSDC);
-    }
+  //// Internal
+  constructor()
+    payable
+    ERC1155("https://app.sorites.xyz/api/v1/tokens/{id}.json")
+    Ownable(msg.sender) {}
 
-    function calculateMintAmountPerUSDC(MarketEvent storage marketEvent, bool tokenTypeYes) private view returns (uint80) {
-        uint64 totalDuration = marketEvent.endTime - marketEvent.startTime;
-        uint64 timeElapsedSoFar = uint64(block.timestamp) - marketEvent.startTime;
-        uint64 elapsedTimeProportion = timeElapsedSoFar / totalDuration;
-        bool timeMoreThanHalfWay = timeElapsedSoFar > (totalDuration >> 1);
-
-        uint256 sigma = timeMoreThanHalfWay
-            ? 8 * (1 - elapsedTimeProportion) ** 4
-            : 1 - 8 * elapsedTimeProportion ** 4;
-
-        uint256 aTokenTotal = tokenTypeYes
-            ? marketEvent.totalYesTokens
-            : marketEvent.totalNoTokens;
-
-        uint256 bTokenTotal = tokenTypeYes
-            ? marketEvent.totalNoTokens
-            : marketEvent.totalYesTokens;
-
-        require(bTokenTotal <= aTokenTotal, "Invalid state");
-
-        uint256 p =
-            aTokenTotal <= bTokenTotal
-            ? aTokenTotal / bTokenTotal
-            : bTokenTotal / aTokenTotal;
-
-        uint256 a = 2 * (1 - p + (1 - p) ** 2);
-
-        uint256 fOfT =
-            p + a * elapsedTimeProportion - (a + p) * elapsedTimeProportion ** 2;
-
-        uint256 swapMultiplier = aTokenTotal <= bTokenTotal ? (sigma * p) / fOfT : (sigma * fOfT) / p;
-
-        if (aTokenTotal <= bTokenTotal) {
-            return uint80(swapMultiplier >= 1 ? swapMultiplier : 1);
-        } else {
-            return 1;
-        }
-    }
-
-    function _mintMarketEventTokens(address minter, uint80 marketEventId, uint80 usdcToDeposit, bool tokenTypeYes) private {
-        MarketEvent storage marketEvent = getMarketEvent(marketEventId);
-    
-        // Assert block time
-        require(marketEvent.endTime > block.timestamp, "MarketEvent is over");
-
-        // Deposit USDC
-        moveUSDC(minter, address(this), usdcToDeposit);
-
-        // Calculate mint rate
-        uint80 mintAmountPerUSDC = calculateMintAmountPerUSDC(marketEvent, tokenTypeYes);
-        uint80 tokensToMint = mintAmountPerUSDC * usdcToDeposit;
-
-        // Mint tokens
-        uint256 marketEventTokenId = tokenTypeYes
-            ? getYesTokenId(marketEventId)
-            : getNoTokenId(marketEventId);
-
-        _mint(minter, marketEventTokenId, tokensToMint, new bytes(0));
-
-        // Update marketEvent (TODO: check that this stores the updates)
-        marketEvent.totalUSDC += usdcToDeposit;
-
-        if (tokenTypeYes) {
-            marketEvent.totalYesTokens += tokensToMint;
-        } else {
-            marketEvent.totalNoTokens += tokensToMint;
-        }
-
-        // Tell the world
-        emit MarketEventResolved(minter, marketEventId, usdcToDeposit, tokenTypeYes);
-    }
-
-    function mintMarketEventTokens(uint80 marketEventId, uint80 usdcToDeposit, bool tokenTypeYes) public {
-        _mintMarketEventTokens(msg.sender, marketEventId, usdcToDeposit, tokenTypeYes);
-    }
-
-    //// IFuturesConsumer
-    function resolveMarketEvent(uint80 marketEventId, bool outcomeWasMet) public {
-        MarketEvent storage marketEvent = getMarketEvent(marketEventId);
-
-        require(marketEvent.futuresContractAddress == msg.sender, "Unauthorised");
-        require(marketEvent.status == MarketEventStatus.InProgress, "MarketEvent not in progress");
-        require(marketEvent.endTime <= block.timestamp, "MarketEvent not over");
-
-        marketEvent.status = outcomeWasMet ? MarketEventStatus.YesWon : MarketEventStatus.NoWon;
-    }
-
-    function createMarketEvent(address minter, uint64 endTime, uint80 usdcToDeposit, bool tokenTypeYes) external returns (uint80) {
-        // Require minimum deposit to create a marketEvent
-        require(usdcToDeposit >= 25 * 10e6, "Not enough USDC");
-
-        // Prevent marketEvents from ending in the past
-        require(endTime >= block.timestamp, "Cannot end in past");
-
-        // Only allow whitelisted Futures Contracts
-        require(futuresContractAddressWhitelist[msg.sender], "Not whitelisted");
-
-        uint80 marketEventId = nextMarketEventId;
-        ++nextMarketEventId;
-
-        marketEvents[marketEventId] = MarketEvent({
-            startTime: uint64(block.timestamp),
-            endTime: endTime,
-
-            totalUSDC: usdcToDeposit,
-            totalYesTokens: 1,
-            totalNoTokens: 1,
-
-            status: MarketEventStatus.InProgress,
-            usdcWonPerWinningToken: 0,
-
-            futuresContractAddress: msg.sender
-        });
-
-        // Mint the tokens
-        _mintMarketEventTokens(minter, marketEventId, usdcToDeposit, tokenTypeYes);
-
-        return marketEventId;
-    }
-
-    //// Internal
-    constructor()
-        payable
-        ERC1155("https://app.sorites.xyz/api/v1/tokens/{id}.json")
-        Ownable(msg.sender) {}
-
-    function setURI(string memory newuri) public onlyOwner {
-        _setURI(newuri);
-    }
+  function setURI(string memory newuri) public onlyOwner {
+    _setURI(newuri);
+  }
 }
